f440503f6a3fbd2617b7a32887d6782becc02761
mswimamtui0 <mswimamtui0@gmail.com>
2026-06-26 20:22:14 +0300

    Fix Vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
+SECRET_KEY = config('SECRET_KEY', default='nfttpfj-8+sl65mig^4-1t_gh+=bujlv-ob^eyla*2!^u%%l-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,10 +1,15 @@
 # core/wsgi.py
 import os
+import sys
+
+# Add project root to path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-# For Vercel
+# Required for Vercel
 app = application
