56b1690e1c33fac32bc6604a880f27ed8f85153a
mswimamtui0 <mswimamtui0@gmail.com>
2026-06-27 16:41:51 +0300

    Fix URLs - use TokenObtainPairView instead of CustomTokenObtainPairView

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from accounts.views import CustomTokenObtainPairView, AuthViewSet
+# Remove this line - it's commented out but still used
+# from accounts.views import CustomTokenObtainPairView, AuthViewSet
+
 from loans.views import LoanViewSet, LoanProductViewSet
 from customers.views import CustomerViewSet
 from customers.views_portal import CustomerAuthViewSet, CustomerPortalViewSet
@@ -35,21 +37,18 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     
-    # JWT Authentication endpoints
-    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT Authentication endpoints - FIXED: Use TokenObtainPairView directly
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    
-    # Custom auth endpoints
-    path('api/auth/', include('accounts.urls')),
-    
     
     # Customer Portal endpoints
     path('api/customer/', include(customer_router.urls)),
     
     # API router
     path('api/', include(router.urls)),
-
+    
+    # Branches
     path('api/', include('branches.urls')),
     
     # Reports
