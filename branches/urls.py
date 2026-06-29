8eb10d94690a8f229f80fa4a3680f189619cb34a
mswimamtui0 <mswimamtui0@gmail.com>
2026-06-29 17:53:35 +0300

    Fix branches urls - remove wrong import

--- branches/urls.py
+++ branches/urls.py
@@ -1,10 +1,10 @@
+# branches/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views_portal import CustomerAuthViewSet, CustomerPortalViewSet
+from .views import BranchViewSet
 
 router = DefaultRouter()
-router.register(r'auth', CustomerAuthViewSet, basename='customer-auth')
-router.register(r'', CustomerPortalViewSet, basename='customer-portal')
+router.register(r'', BranchViewSet, basename='branch')
 
 urlpatterns = [
     path('', include(router.urls)),
