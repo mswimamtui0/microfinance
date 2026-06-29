d7f7c520598f5a0bfe3f9ed3b91757342dfbb152
mswimamtui0 <mswimamtui0@gmail.com>
2026-06-29 13:11:06 +0300

    Auto-run migrations on startup

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,14 +1,15 @@
+# core/wsgi.py
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Try to run migrations, but don't fail if they don't work
+# Run migrations automatically
 try:
-    from django.core.management import call_command
     call_command('migrate', verbosity=0)
-except Exception:
-    pass  # Silently ignore migration errors
+    print("Migrations applied successfully!")
+except Exception as e:
+    print(f"Migration error: {e}")
 
 application = get_wsgi_application()
